de7c28a260f16d432826b1c468d2cbbfd113f0fb
Nguyen <nguyen.icome2kill.nguyen@gmail.com>
2013-06-26 15:54:44 +0700

    Fixed typo error

--- db/migrate/20130624082409_create_relationships.rb
+++ db/migrate/20130624082409_create_relationships.rb
@@ -8,6 +8,6 @@ class CreateRelationships < ActiveRecord::Migration
     end
     add_index :relationships, :followed_id
     add_index :relationships, :follower_id
-    add_index :relationships, [:followed_id, :follower_id], unique: trues
+    add_index :relationships, [:followed_id, :follower_id], unique: true
   end
 end
